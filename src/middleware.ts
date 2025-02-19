980dc208366831bf98503c717e9071f7ffeb6da2
Wojciech Kosmalski <jawmen999@gmail.com>
2025-02-19 18:52:52 +0100

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,12 @@
 import { auth } from "@/auth";
 
 export const middleware = auth((request) => {
-  if (request.auth == null && request.nextUrl.pathname !== "/auth/login") {
+  if (request.auth == null) {
     const newUrl = new URL("/auth/login", request.nextUrl.origin);
     return Response.redirect(newUrl);
   }
 });
 
 export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|$).*)",
-    "/dashboard",
-  ],
+  matcher: ["/((?!api|auth|event|_next/static|_next/image|favicon.ico|$).*)"],
 };
