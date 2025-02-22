0d15c0564abe95af8963fb7cf598208267aa272a
Maciej Malinowski <mejsiejdev@gmail.com>
2025-02-22 23:52:59 +0100

    feat: make modal layout work in dark mode

--- src/app/dashboard/@modals/(.)event/create/layout.tsx
+++ src/app/dashboard/@modals/(.)event/create/layout.tsx
@@ -7,8 +7,8 @@ export default function CreateEventFormLayout({
   children: Readonly<React.ReactNode>;
 }) {
   return (
-    <div className="absolute left-0 top-0 z-40 flex min-h-screen w-full flex-col items-center justify-center bg-black/75">
-      <div className="flex w-full max-w-3xl flex-col items-end rounded-xl bg-white p-6">
+    <div className="absolute left-0 top-0 z-40 flex min-h-screen w-full flex-col items-center justify-center bg-black/80">
+      <div className="flex w-full max-w-3xl flex-col items-end rounded-xl border bg-background p-6 shadow-lg">
         <Link
           href="/dashboard"
           className="absolute rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
