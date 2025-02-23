5916e8cbf973237a99260cce02bf2245bb5ee66e
Maciej Malinowski <mejsiejdev@gmail.com>
2025-02-23 01:05:41 +0100

    feat: keep values when going to previous step

--- src/app/dashboard/event/create/state.ts
+++ src/app/dashboard/event/create/state.ts
@@ -2,4 +2,32 @@
 
 import { atom } from "jotai";
 
-export const eventAtom = atom();
+interface Event {
+  name: string;
+  description: string | undefined;
+  startDate: Date;
+  endDate: Date;
+  lat: number;
+  long: number;
+  organizer: string | undefined;
+  image: string;
+  color: string;
+  participantsNumber: number;
+  links: string[];
+  slug: string;
+}
+
+export const eventAtom = atom<Event>({
+  name: "",
+  description: "",
+  startDate: new Date(),
+  endDate: new Date(),
+  lat: 0,
+  long: 0,
+  organizer: "",
+  image: "",
+  color: "#3672fd",
+  participantsNumber: 1,
+  links: [],
+  slug: "",
+});
