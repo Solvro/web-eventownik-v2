e4d67cabff1fce9d0bb9ee309b440e93d81d30e4
Wojciech Kosmalski <jawmen999@gmail.com>
2025-02-25 18:10:14 +0100

    refactor: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,35 +3,17 @@ import { NextResponse } from "next/server";
 
 import { verifySession } from "@/lib/session";
 
-// 1. Specify protected and public routes
-const protectedRoutes = new Set(["/dashboard"]);
-const publicRoutes = new Set(["/auth/login", "/auth/register", "/"]);
-
 export async function middleware(request: NextRequest) {
   // 2. Check if the current route is protected or public
-  const path = request.nextUrl.pathname;
-  const isProtectedRoute = protectedRoutes.has(path);
-  const isPublicRoute = publicRoutes.has(path);
   const session = await verifySession();
 
   // 4. Redirect to /auth/login if the user is not authenticated
-  if (isProtectedRoute && session?.bearerToken == null) {
+  if (session?.bearerToken == null) {
     return NextResponse.redirect(new URL("/auth/login", request.nextUrl));
   }
-
-  // 5. Redirect to /dashboard if the user is authenticated
-  if (
-    isPublicRoute &&
-    session?.bearerToken != null &&
-    !request.nextUrl.pathname.startsWith("/dashboard")
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
-  }
-
-  return NextResponse.next();
 }
 
 // Routes Middleware should not run on
 export const config = {
-  matcher: [`/((?!api|_next/static|_next/image|.*\\.png$).*)`],
+  matcher: ["/((?!api|auth|event|_next/static|_next/image|favicon.ico|$).*)"],
 };
