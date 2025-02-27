1f61bc4cd21aec700ea0c06ec600700c1c073d59
Maciej Malinowski <mejsiejdev@gmail.com>
2025-02-27 17:54:40 +0100

    fix: send data in `formData` format
    dates with timezones don't work 👍

--- src/app/dashboard/@modals/(.)event/create/actions.ts
+++ src/app/dashboard/@modals/(.)event/create/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { formatISO } from "date-fns";
+import { formatISO, formatISO9075 } from "date-fns";
 
 import { API_URL } from "@/lib/api";
 import { verifySession } from "@/lib/session";
@@ -9,31 +9,55 @@ import type { Event } from "./state";
 
 export async function saveEvent(event: Event) {
   const { bearerToken } = await verifySession();
+  const formData = new FormData();
+  formData.append("name", event.name);
+  formData.append("description", event.description ?? "");
+  formData.append("organizer", event.organizer ?? "");
+  formData.append("slug", event.slug);
+  formData.append(
+    "startDate",
+    formatISO9075(event.startDate, { representation: "complete" }),
+  );
+  formData.append(
+    "endDate",
+    formatISO9075(event.endDate, { representation: "complete" }),
+  );
+  formData.append("lat", event.lat.toString());
+  formData.append("long", event.long.toString());
+  formData.append("primaryColor", event.color);
+  formData.append("participantsCount", event.participantsNumber.toString());
+  formData.append("photo", event.image);
   const data = await fetch(`${API_URL}/events`, {
     method: "POST",
     headers: {
-      "Content-Type": "multipart/form-data",
       Authorization: `Bearer ${bearerToken}`,
     },
-    body: JSON.stringify({
-      name: event.name,
-      description: event.description,
-      organizer: event.organizer,
-      slug: event.slug,
-      startDate: formatISO(event.startDate, { representation: "complete" }),
-      endDate: formatISO(event.endDate, { representation: "complete" }),
-      lat: event.lat,
-      long: event.long,
-      primaryColor: event.color,
-      participantsCount: event.participantsNumber,
-      photo: event.image,
-    }),
+    body: formData,
   }).then(async (response) => {
-    if (response.status !== 201) {
-      return response.json() as Promise<{
-        errors: { message: string }[];
-      }>;
-    }
+    return response.status === 201
+      ? (response.json() as Promise<{
+          id: string;
+        }>)
+      : (response.json() as Promise<{
+          errors: { message: string }[];
+        }>);
   });
+  if ("id" in data) {
+    await Promise.all(
+      event.attributes.map(async (attribute) =>
+        fetch(`${API_URL}/events/${data.id}/attributes`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${bearerToken}`,
+          },
+          body: JSON.stringify({
+            name: attribute.name,
+            type: attribute.type,
+          }),
+        }),
+      ),
+    );
+  }
   return data;
 }
