ef9b402b6b6e9db577be31789fa5f616143be86d
Wojciech Kosmalski <jawmen999@gmail.com>
2025-02-28 19:12:00 +0100

    feat: add photo url env

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,10 @@
 export const API_URL = process.env.EVENTOWNIK_API ?? "";
+export const PHOTO_URL = process.env.PHOTO_URL ?? "";
 
 if (API_URL === "") {
   throw new Error("EVENTOWNIK_API was not set in enviroment variables!");
+}
+
+if (PHOTO_URL === "") {
+  throw new Error("PHOTO_URL was not set in enviroment variables!");
 }
