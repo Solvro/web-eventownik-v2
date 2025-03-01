d345f0e50ec61093efcb24233ab0c34fd3163386
Wojciech Kosmalski <106556099+chewmanji@users.noreply.github.com>
2025-03-01 23:12:15 +0100

    feat: add participants table (#29)
    * feat: display participants data in table
    
    * feat: add sorting, filtering, pagination
    
    * feat: add deleting participant
    
    * refactor: files structure
    
    * feat: add updating participant (almost)
    
    * feat: add working participant updating
    
    * fix: typos and fixes
    
    * refactor: edit form cleanups
    
    * chore: remove mock data from page

---
+++ src/app/dashboard/events/[id]/participants/table/utils.tsx
@@ -0,0 +1,116 @@
+import type { Column, SortDirection, Table } from "@tanstack/react-table";
+import { ArrowDown, ArrowUp, Filter } from "lucide-react";
+import type { ReactNode } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import type { AttributeType } from "@/types/attributes";
+import type { FlattenedParticipant } from "@/types/participant";
+
+export function SortButton({
+  sortingDirection,
+  column,
+  children,
+}: {
+  sortingDirection: false | SortDirection;
+  column: Column<FlattenedParticipant, string | number | boolean | Date | null>;
+  children: ReactNode;
+}) {
+  return (
+    <Button
+      variant="ghost"
+      onClick={() => {
+        column.toggleSorting(sortingDirection === "asc");
+      }}
+    >
+      {children}
+    </Button>
+  );
+}
+
+export function SortIcon({
+  sortingDirection,
+}: {
+  sortingDirection: false | SortDirection;
+}) {
+  if (sortingDirection === false) {
+    return null;
+  }
+  if (sortingDirection === "desc") {
+    return <ArrowDown />;
+  }
+  return <ArrowUp />;
+}
+
+export function FilterButton({
+  attributeType,
+  options,
+  column,
+}: {
+  attributeType: AttributeType;
+  options: string[] | null;
+  column: Column<FlattenedParticipant, string | number | boolean | Date | null>;
+}) {
+  if (
+    attributeType === "checkbox" ||
+    (attributeType === "select" && options !== null)
+  ) {
+    const filterValues =
+      (column.getFilterValue() as string[] | undefined) ?? [];
+
+    if (attributeType === "checkbox" && options === null) {
+      options = ["true", "false"];
+    }
+
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant={filterValues.length === 0 ? "ghost" : "outline"}
+            size="icon"
+          >
+            <Filter strokeWidth={filterValues.length === 0 ? 2 : 3} />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {options?.map((option) => {
+            return (
+              <DropdownMenuCheckboxItem
+                key={option}
+                onSelect={(event) => {
+                  event.preventDefault();
+                }}
+                checked={filterValues.includes(option)}
+                onCheckedChange={(checked) => {
+                  const newFilterValues = checked
+                    ? [...filterValues, option]
+                    : filterValues.filter((value) => value !== option);
+                  column.setFilterValue(
+                    newFilterValues.length > 0 ? newFilterValues : [],
+                  );
+                }}
+              >
+                {option}
+              </DropdownMenuCheckboxItem>
+            );
+          })}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  }
+  return null;
+}
+
+export function getPaginationInfoText(table: Table<FlattenedParticipant>) {
+  const { pageIndex, pageSize } = table.getState().pagination;
+  return `${
+    table.getPaginationRowModel().rows.length === 0
+      ? "0"
+      : (pageIndex * pageSize + 1).toString()
+  }-${Math.min(pageSize * pageIndex + pageSize, table.getRowCount()).toString()} z ${table.getRowCount().toString()}`;
+}
--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -7,7 +7,8 @@ export default function EventListPage() {
     <div className="flex flex-col gap-4">
       <h1 className="text-3xl font-bold">Wszystkie wydarzenia - 0</h1>
       <Button className="max-w-fit" asChild>
-        <Link href="/dashboard/events/1234">
+        {/* TODO display list of events */}
+        <Link href="/dashboard/events/2">
           Kliknij by przejść do widoku zarządzania eventem
         </Link>
       </Button>
