a4b70d228959d4b821c17730e11fb7ac658ea7a4
Antoni Czaplicki <antekczaplicki@gmail.com>
2025-03-03 13:08:38 +0100

    fix: login automatically after registration

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -26,6 +26,9 @@ export async function register(values: z.infer<typeof registerFormSchema>) {
     console.error("Error when registering", response);
     return response.json() as Promise<AuthErrorResponse>;
   });
+  if ("token" in data) {
+    await createSession({ bearerToken: data.token });
+  }
   return data;
 }
 
