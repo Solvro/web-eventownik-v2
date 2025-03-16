8c8979d049a73c102b49c905afc23c5a3d0d69dd
Maciej Malinowski <mejsiejdev@gmail.com>
2025-03-16 14:22:21 +0100

    feat: start working on event settings page

--- src/app/dashboard/events/[id]/settings/page.tsx
+++ src/app/dashboard/events/[id]/settings/page.tsx
@@ -1,3 +1,44 @@
-export default function DashboardEventSettingsPage() {
-  return <h1 className="text-3xl font-bold">Edytuj wydarzenie</h1>;
+import { notFound } from "next/navigation";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { API_URL } from "@/lib/api";
+import { verifySession } from "@/lib/session";
+import type { Event } from "@/types/event";
+
+export default async function DashboardEventSettingsPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const session = await verifySession();
+  if (session == null || typeof session.bearerToken !== "string") {
+    notFound();
+  }
+  const { bearerToken } = session;
+  const { id } = await params;
+  const response = await fetch(`${API_URL}/events/${id}`, {
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+    },
+  });
+  if (!response.ok) {
+    notFound();
+  }
+  const event = (await response.json()) as Event;
+  console.log(event);
+  return (
+    <div className="space-y-8">
+      <h1 className="text-3xl font-bold">Edytuj wydarzenie</h1>
+      <Tabs defaultValue="account">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
 }
