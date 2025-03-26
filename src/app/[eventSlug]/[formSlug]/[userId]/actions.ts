89c392b92ea8e0b79385396b4f738c91bccb52d3
Maciej Malinowski <mejsiejdev@gmail.com>
2025-03-26 18:45:42 +0100

    feat: create page for next event forms (#60)
    * feat: create page for next event forms
    
    * refactor: change the way the attributes are passed
    
    * refactor: fix lint issues and add correct types
    
    * feat: remove email fetching
    
    * refactor: use event's slug instead of id
    
    * fix: lint errors
    
    * fix: implement ellipsis suggestions
    
    * chore: fix lint
    
    ---------
    
    Co-authored-by: Antoni Czaplicki <56671347+Antoni-Czaplicki@users.noreply.github.com>

---
+++ src/app/[eventSlug]/[formSlug]/[userId]/actions.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { API_URL } from "@/lib/api";
+
+interface ErrorObject {
+  rule: string;
+  field: string;
+}
+
+interface ErrorResponse {
+  errors: ErrorObject[];
+}
+
+type Values = Record<string, unknown>;
+
+export async function submitForm(
+  values: Values,
+  formId: string,
+  eventSlug: string,
+  userId: string,
+) {
+  try {
+    const response = await fetch(
+      `${API_URL}/events/${eventSlug}/forms/${formId}/submit`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          participantSlug: userId,
+          ...values,
+        }),
+      },
+    );
+    if (!response.ok) {
+      console.error("Error when saving form", response);
+      const errorData = (await response.json()) as ErrorResponse;
+      console.error("Error when saving form", errorData.errors);
+      return { success: false, errors: errorData.errors };
+    }
+  } catch (error) {
+    console.error("Error when saving form", error);
+    return { success: false };
+  }
+
+  return { success: true };
+}
---
+++ src/app/[eventSlug]/[formSlug]/[userId]/form-generator.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { AttributeInput } from "@/components/attribute-input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/hooks/use-toast";
+import { getSchemaObjectForAttributes } from "@/lib/utils";
+import type { Attribute } from "@/types/attributes";
+
+import { submitForm } from "./actions";
+
+export function FormGenerator({
+  attributes,
+  formId,
+  eventSlug,
+  userId,
+}: {
+  attributes: Attribute[];
+  formId: string;
+  eventSlug: string;
+  userId: string;
+}) {
+  // generate schema for form based on attributes
+  const FormSchema = z.object({
+    ...getSchemaObjectForAttributes(attributes.sort((a, b) => a.id - b.id)), // TODO: change to order after backend changes
+  });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const { toast } = useToast();
+
+  async function onSubmit(values: z.infer<typeof FormSchema>) {
+    try {
+      const result = await submitForm(values, formId, eventSlug, userId);
+      if (!result.success) {
+        form.setError("root", {
+          type: "manual",
+          message:
+            "Zapisanie danych formularza nie powiodło się.\nSpróbuj ponownie później",
+        });
+        toast({
+          variant: "destructive",
+          title: "Zapisanie danych formularza nie powiodło się",
+          description: "Spróbuj ponownie później",
+        });
+      }
+    } catch (error) {
+      console.error("Form submission failed", error);
+      toast({
+        variant: "destructive",
+        title: "Zapisanie danych formularza nie powiodło się",
+        description: "Błąd serwera",
+      });
+    }
+  }
+
+  if (form.formState.isSubmitSuccessful) {
+    return (
+      <div>
+        <h2 className="text-center text-xl font-bold text-green-500 md:text-2xl">
+          Twoja odpowiedź została zapisana!
+        </h2>
+        <br />
+        <div className="text-center">
+          <Button
+            onClick={() => {
+              form.reset();
+            }}
+          >
+            Uzupełnij kolejny formularz
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full max-w-sm space-y-4"
+      >
+        {attributes.map((attribute) => (
+          <FormField
+            key={attribute.id}
+            control={form.control}
+            name={attribute.id.toString()}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{attribute.name}</FormLabel>
+                <FormControl>
+                  <AttributeInput attribute={attribute} field={field} />
+                </FormControl>
+                <FormMessage className="text-sm text-red-500">
+                  {form.formState.errors[attribute.id.toString()]?.message}
+                </FormMessage>
+              </FormItem>
+            )}
+          />
+        ))}
+        {form.formState.errors.root?.message != null && (
+          <FormMessage className="text-center text-sm whitespace-break-spaces text-red-500">
+            {form.formState.errors.root.message}
+          </FormMessage>
+        )}
+        <Button
+          type="submit"
+          disabled={form.formState.isSubmitting}
+          className="w-full"
+        >
+          {form.formState.isSubmitting ? (
+            <>
+              <Loader2 className="animate-spin" /> Zapisywanie...
+            </>
+          ) : (
+            "Zapisz"
+          )}
+        </Button>
+      </form>
+    </Form>
+  );
+}
