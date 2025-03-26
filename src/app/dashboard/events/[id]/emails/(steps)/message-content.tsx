1510c448f8f2294b3ee19de937cacccc40f67d83
Maciej Król <maciekgplay@gmail.com>
2025-03-26 11:50:36 +0100

    feat: add tiptap editor for message content

--- src/app/dashboard/events/[id]/emails/(steps)/message-content.tsx
+++ src/app/dashboard/events/[id]/emails/(steps)/message-content.tsx
@@ -1,8 +1,28 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Placeholder } from "@tiptap/extension-placeholder";
+import { TextAlign } from "@tiptap/extension-text-align";
+import { EditorContent, useEditor } from "@tiptap/react";
+import { StarterKit } from "@tiptap/starter-kit";
 import { useAtom } from "jotai";
-import { ArrowLeft, Loader, Save, TextIcon } from "lucide-react";
+import {
+  AlignCenter,
+  AlignJustify,
+  AlignLeft,
+  AlignRight,
+  ArrowLeft,
+  Bold,
+  Code2,
+  Heading1,
+  Heading2,
+  Heading3,
+  Italic,
+  Loader,
+  Save,
+  TextIcon,
+} from "lucide-react";
+import { useRef } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -18,15 +38,171 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { useToast } from "@/hooks/use-toast";
 
 import { createEventEmailTemplate } from "../actions";
+
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 
 const EventEmailTemplateContentSchema = z.object({
   name: z.string().nonempty({ message: "Tytuł nie może być pusty." }),
   content: z.string().nonempty({ message: "Treść nie może być pusta." }),
 });
+
+function Editor({
+  form,
+  onChange,
+}: {
+  form: ReturnType<
+    typeof useForm<z.infer<typeof EventEmailTemplateContentSchema>>
+  >;
+  onChange: (value: string) => void;
+}) {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      Placeholder.configure({ placeholder: "Napisz wiadomość" }),
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+    ],
+    content: form.getValues("content"),
+    onUpdate: ({ editor: onUpdateEditor }) => {
+      onChange(onUpdateEditor.getHTML());
+    },
+    editorProps: {
+      attributes: {
+        class:
+          "pb-4 focus:outline-none cursor-text max-h-[200px] leading-relaxed",
+      },
+      handleKeyDown: (_, event) => {
+        if (event.key === "Enter") {
+          return false;
+        }
+      },
+    },
+  });
+
+  return (
+    <div className="min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-base shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">
+      <EditorMenuBar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  );
+}
+
+function EditorMenuBar({ editor }: { editor: ReturnType<typeof useEditor> }) {
+  return (
+    <div className="flex gap-2 pb-4">
+      <Button
+        size="icon"
+        variant={editor?.isActive("bold") ? "default" : "ghost"}
+        type="button"
+        onClick={() => editor?.chain().focus().toggleBold().run()}
+        title="Tekst pogrubiony"
+      >
+        <Bold />
+      </Button>
+      <Button
+        size="icon"
+        variant={editor?.isActive("italic") ? "default" : "ghost"}
+        type="button"
+        onClick={() => editor?.chain().focus().toggleItalic().run()}
+        title="Tekst pochyły"
+      >
+        <Italic />
+      </Button>
+      <Button
+        size="icon"
+        variant={editor?.isActive("code") ? "default" : "ghost"}
+        type="button"
+        onClick={() => editor?.chain().focus().toggleCode().run()}
+        title="Kod (czcionka mono)"
+      >
+        <Code2 />
+      </Button>
+      <Button
+        size="icon"
+        variant={
+          editor?.isActive("heading", { level: 1 }) ? "default" : "ghost"
+        }
+        type="button"
+        onClick={() =>
+          editor?.chain().focus().toggleHeading({ level: 1 }).run()
+        }
+        title="Nagłówek stopnia pierwszego"
+      >
+        <Heading1 />
+      </Button>
+      <Button
+        size="icon"
+        variant={
+          editor?.isActive("heading", { level: 2 }) ? "default" : "ghost"
+        }
+        type="button"
+        onClick={() =>
+          editor?.chain().focus().toggleHeading({ level: 2 }).run()
+        }
+        title="Nagłówek stopnia drugiego"
+      >
+        <Heading2 />
+      </Button>
+      <Button
+        size="icon"
+        variant={
+          editor?.isActive("heading", { level: 3 }) ? "default" : "ghost"
+        }
+        type="button"
+        onClick={() =>
+          editor?.chain().focus().toggleHeading({ level: 3 }).run()
+        }
+        title="Nagłówek stopnia trzeciego"
+      >
+        <Heading3 />
+      </Button>
+      <Button
+        size="icon"
+        type="button"
+        onClick={() => editor?.chain().focus().setTextAlign("left").run()}
+        variant={editor?.isActive({ textAlign: "left" }) ? "default" : "ghost"}
+        title="Wyrównanie do lewej"
+      >
+        <AlignLeft />
+      </Button>
+      <Button
+        size="icon"
+        type="button"
+        onClick={() => editor?.chain().focus().setTextAlign("center").run()}
+        variant={
+          editor?.isActive({ textAlign: "center" }) ? "default" : "ghost"
+        }
+        title="Wyrównanie do środka"
+      >
+        <AlignCenter />
+      </Button>
+      <Button
+        size="icon"
+        type="button"
+        onClick={() => editor?.chain().focus().setTextAlign("right").run()}
+        variant={editor?.isActive({ textAlign: "right" }) ? "default" : "ghost"}
+        title="Wyrównanie w prawo"
+      >
+        <AlignRight />
+      </Button>
+      <Button
+        size="icon"
+        type="button"
+        onClick={() => editor?.chain().focus().setTextAlign("justify").run()}
+        variant={
+          editor?.isActive({ textAlign: "justify" }) ? "default" : "ghost"
+        }
+        title="Justuj"
+      >
+        <AlignJustify />
+      </Button>
+    </div>
+  );
+}
 
 function MessageContentForm({
   eventId,
@@ -48,6 +224,8 @@ function MessageContentForm({
       content: newEmailTemplate.content,
     },
   });
+
+  const formRef = useRef<HTMLFormElement | null>(null);
 
   function saveEdits() {
     setNewEmailTemplate((previous) => {
@@ -95,7 +273,11 @@ function MessageContentForm({
       title="Krok 2"
     >
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          ref={formRef}
+          className="space-y-8"
+        >
           <FormField
             control={form.control}
             name="name"
@@ -119,12 +301,7 @@ function MessageContentForm({
             render={({ field }) => (
               <FormItem>
                 <FormLabel>Treść wiadomości</FormLabel>
-                <FormControl>
-                  <Textarea
-                    placeholder="Lorem ipsum dolor sit amet"
-                    {...field}
-                  />
-                </FormControl>
+                <Editor form={form} onChange={field.onChange} />
                 <FormMessage>{form.formState.errors.name?.message}</FormMessage>
               </FormItem>
             )}
--- src/app/dashboard/events/[id]/emails/template-entry.tsx
+++ src/app/dashboard/events/[id]/emails/template-entry.tsx
@@ -2,7 +2,18 @@ import { SquarePen } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import { EMAIL_TRIGGERS } from "@/lib/emails";
 import type { EventEmail } from "@/types/emails";
+
+function EmailTriggerLabel({ trigger }: { trigger: string }) {
+  const target = EMAIL_TRIGGERS.find((t) => t.value === trigger);
+
+  if (target === undefined) {
+    return null;
+  }
+
+  return <p className="text-muted-foreground">{target.name}</p>;
+}
 
 function EmailTemplateEntry({
   emailTemplate,
@@ -13,18 +24,18 @@ function EmailTemplateEntry({
   return (
     <div className="flex h-64 w-64 flex-col justify-between rounded-md border border-slate-500 p-4">
       <div className="flex items-center justify-end">
-        {/* TODO: Implement form edit view */}
+        {/* TODO: Implement mail edit view */}
         <Button variant="ghost" size="icon" asChild>
           <Link href={`templates/${emailTemplate.id.toString()}`}>
             <SquarePen />
             <span className="sr-only">Edytuj szablon maila</span>
           </Link>
         </Button>
-        {/* todo: deleting */}
+        {/* TODO: Implement mail deletion */}
       </div>
-      <div className="flex grow flex-col items-center justify-center gap-2">
+      <div className="flex grow flex-col items-center justify-center gap-2 text-center">
         <p className="text-lg font-bold">{emailTemplate.name}</p>
-        <p className="text-muted-foreground">Lorem ipsum dolor sit amet.</p>
+        <EmailTriggerLabel trigger={emailTemplate.trigger} />
       </div>
     </div>
   );
