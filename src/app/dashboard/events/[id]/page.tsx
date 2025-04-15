aa4d54f1e2054244efcbe1f1eb865dd599456a83
Tymon Jędryczka <65811982+jedryczkatymon@users.noreply.github.com>
2025-04-15 19:58:34 +0200

    Linki na stronie wydarzenia (#102)
    * feat: add social media links to the dashboard and event page
    
    * refactor: move some things around
    
    ---------
    
    Co-authored-by: Antoni Czaplicki <56671347+Antoni-Czaplicki@users.noreply.github.com>
    Co-authored-by: Antoni Czaplicki <antekczaplicki@gmail.com>

--- src/app/dashboard/events/[id]/page.tsx
+++ src/app/dashboard/events/[id]/page.tsx
@@ -1,3 +1,4 @@
+import * as Tooltip from "@radix-ui/react-tooltip";
 import { format } from "date-fns";
 import {
   Calendar1,
@@ -10,10 +11,19 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
+import {
+  FaDiscord,
+  FaFacebookF,
+  FaGlobe,
+  FaInstagram,
+  FaTiktok,
+  FaYoutube,
+} from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 
 import EventPhotoPlaceholder from "@/../public/event-photo-placeholder.png";
+import { ShareButton } from "@/components/share-button";
 import { Button } from "@/components/ui/button";
-import { ShareButton } from "@/components/ui/share-button";
 import { API_URL, PHOTO_URL } from "@/lib/api";
 import { verifySession } from "@/lib/session";
 import type { Event } from "@/types/event";
@@ -76,6 +86,44 @@ export default async function DashboardEventPage({
         {event.description != null && event.description.trim() !== "" ? (
           <p className="whitespace-pre-line">{event.description}</p>
         ) : null}
+        <div className="flex gap-1">
+          {event.socialMediaLinks != null && event.socialMediaLinks.length > 0
+            ? event.socialMediaLinks.map((link) => (
+                <Tooltip.Provider key={link} delayDuration={0}>
+                  <Tooltip.Root>
+                    <Tooltip.Trigger asChild>
+                      <Button asChild variant={"ghost"} className="size-10">
+                        <Link href={link} target="_blank">
+                          {link.includes("facebook.com") ? (
+                            <FaFacebookF className="!size-6" />
+                          ) : link.includes("instagram.com") ? (
+                            <FaInstagram className="!size-6" />
+                          ) : link.includes("tiktok.com") ? (
+                            <FaTiktok className="!size-6" />
+                          ) : link.includes("discord.com") ? (
+                            <FaDiscord className="!size-6" />
+                          ) : link.includes("youtube.com") ? (
+                            <FaYoutube className="!size-6" />
+                          ) : link.includes("google.com/maps") ? (
+                            <FaLocationDot className="!size-6" />
+                          ) : (
+                            <FaGlobe className="!size-6" />
+                          )}
+                        </Link>
+                      </Button>
+                    </Tooltip.Trigger>
+                    <Tooltip.Content
+                      className="rounded bg-gray-900 px-2 py-1 text-sm text-white"
+                      sideOffset={5}
+                    >
+                      {link}
+                      <Tooltip.Arrow className="fill-gray-900" />
+                    </Tooltip.Content>
+                  </Tooltip.Root>
+                </Tooltip.Provider>
+              ))
+            : null}
+        </div>
         <div className="flex flex-col gap-2 md:flex-row">
           <Button asChild>
             <Link href={`/dashboard/events/${id}/settings`}>
