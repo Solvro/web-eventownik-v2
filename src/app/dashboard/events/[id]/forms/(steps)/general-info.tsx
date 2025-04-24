8858cf506ba28270e7887eb8d4b259a312f7fda4
Tymon Jędryczka <jedryczkatymon@gmail.com>
2025-04-24 22:16:40 +0200

    fix: increase the height of the textarea in forms

--- src/app/dashboard/events/[id]/forms/(steps)/general-info.tsx
+++ src/app/dashboard/events/[id]/forms/(steps)/general-info.tsx
@@ -231,7 +231,7 @@ function GeneralInfoForm({ goToNextStep }: { goToNextStep: () => void }) {
                     <FormControl>
                       <Textarea
                         {...field}
-                        className="h-5/6 resize-none"
+                        className="h-62 resize-none"
                         disabled={form.formState.isSubmitting}
                       />
                     </FormControl>
