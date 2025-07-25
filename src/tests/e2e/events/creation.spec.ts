aa6ddbcc5297811be27ce78d162c626d475da2a6
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2025-07-26 00:37:04 +0200

    test: split the tests to separate files
    it will enable tests that don't need to run in serial mode to run in parallel

--- src/tests/e2e/events/creation.spec.ts
+++ src/tests/e2e/events/creation.spec.ts
@@ -59,60 +59,39 @@ test("should create an event", async ({ page }) => {
   await expect(page.getByRole("main")).toContainText(testData.description);
 });
 
-test.describe("form error checks", () => {
-  test("should throw errors if the fields are not set correctly", async ({
-    page,
-  }) => {
-    // Open up the event creation modal
-    await page.getByRole("button", { name: "Stwórz wydarzenie" }).click();
-    // Try to go to the next step without filling in the name
-    await page.getByRole("button", { name: "Dalej" }).click();
-    // Verify the error message for missing name is displayed
-    await expect(page.getByText(/nazwa nie może być pusta/i)).toBeVisible();
-    // Fill in the name
-    await page.getByRole("textbox", { name: "Nazwa" }).fill(testData.name);
-    // Set a time in the past and try to go to the next step
-    await page.locator('input[name="startTime"]').fill("09:00");
-    await page.getByRole("button", { name: "Dalej" }).click();
-    // Verify the error message for invalid start date is displayed
-    await expect(page.getByText(/nie może być w przeszłości/i)).toBeVisible();
-    // Set the start date and try to go to the next step without setting the end date
-    await page.getByRole("button", { name: "Data i godzina" }).click();
-    await page.getByRole("button", { name: "Wednesday, August 13th," }).click();
-    await page.getByRole("button", { name: "Dalej" }).click();
-    // Verify the error message for invalid end date is displayed
-    await expect(
-      page.getByText(/musi być po dacie rozpoczęcia/i),
-    ).toBeVisible();
-  });
-
-  test("should throw an error if the event slug is already used", async ({
-    page,
-  }) => {
-    // Open up the event creation modal
-    await page.getByRole("button", { name: "Stwórz wydarzenie" }).click();
-    // Fill in the event details
-    await page.getByRole("textbox", { name: "Nazwa" }).fill(testData.name);
-    await page.getByRole("button", { name: "Data i godzina" }).click();
-    await page.getByRole("button", { name: "Wednesday, August 13th," }).click();
-    await page.locator('input[name="startTime"]').fill("13:30");
-    await page.getByRole("button", { name: "August 8th," }).first().click();
-    await page
-      .getByRole("button", { name: "Thursday, August 14th," })
-      .last()
-      .click();
-    await page.locator('input[name="endTime"]').fill("15:30");
-    // Skip the custom fields steps
-    await page.getByRole("button", { name: "Dalej" }).click();
-    await page.getByText("Spersonalizuj wydarzenie").waitFor();
-    // Fill in the slug that is already used
-    await page.getByRole("textbox", { name: "Slug" }).fill(testData.slug);
-    // Try to go to the next step
-    await page.getByRole("button", { name: "Dalej" }).click();
-    // Verify the error message is displayed
-    await page.getByText(/zajęty/i).waitFor();
-    await expect(page.getByText(/zajęty/i)).toBeVisible();
-  });
+/**
+ * NOTE: This is that one test which needs to be run after the event creation test,
+ * because it checks for the slug that is already used.
+ * The problem is that if this one fails somehow, the deletion test will not be able to run,
+ * and the event will remain in the database, making the next test runs fail.
+ * For now I have no idea how to solve this issue - Maciej
+ */
+test("should throw an error if the event slug is already used", async ({
+  page,
+}) => {
+  // Open up the event creation modal
+  await page.getByRole("button", { name: "Stwórz wydarzenie" }).click();
+  // Fill in the event details
+  await page.getByRole("textbox", { name: "Nazwa" }).fill(testData.name);
+  await page.getByRole("button", { name: "Data i godzina" }).click();
+  await page.getByRole("button", { name: "Wednesday, August 13th," }).click();
+  await page.locator('input[name="startTime"]').fill("13:30");
+  await page.getByRole("button", { name: "August 8th," }).first().click();
+  await page
+    .getByRole("button", { name: "Thursday, August 14th," })
+    .last()
+    .click();
+  await page.locator('input[name="endTime"]').fill("15:30");
+  // Skip the custom fields steps
+  await page.getByRole("button", { name: "Dalej" }).click();
+  await page.getByText("Spersonalizuj wydarzenie").waitFor();
+  // Fill in the slug that is already used
+  await page.getByRole("textbox", { name: "Slug" }).fill(testData.slug);
+  // Try to go to the next step
+  await page.getByRole("button", { name: "Dalej" }).click();
+  // Verify the error message is displayed
+  await page.getByText(/zajęty/i).waitFor();
+  await expect(page.getByText(/zajęty/i)).toBeVisible();
 });
 
 test("should delete an event", async ({ page }) => {
