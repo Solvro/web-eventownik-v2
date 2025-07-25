84cafc828f63b48755dc989502e8538e6f9d39fc
Maciej Malinowski <mejsiejdev@gmail.com>
2025-07-25 23:39:33 +0200

    test: create a test for event's required fields

--- src/tests/e2e/dashboard.spec.ts
+++ src/tests/e2e/dashboard.spec.ts
@@ -59,6 +59,30 @@ test("should create an event", async ({ page }) => {
   await expect(page.getByRole("main")).toContainText(testData.description);
 });
 
+test("should throw errors if the fields are not set correctly", async ({
+  page,
+}) => {
+  // Open up the event creation modal
+  await page.getByRole("button", { name: "Stwórz wydarzenie" }).click();
+  // Try to go to the next step without filling in the name
+  await page.getByRole("button", { name: "Dalej" }).click();
+  // Verify the error message for missing name is displayed
+  await expect(page.getByText(/nazwa nie może być pusta/i)).toBeVisible();
+  // Fill in the name
+  await page.getByRole("textbox", { name: "Nazwa" }).fill(testData.name);
+  // Set a time in the past and try to go to the next step
+  await page.locator('input[name="startTime"]').fill("09:00");
+  await page.getByRole("button", { name: "Dalej" }).click();
+  // Verify the error message for invalid start date is displayed
+  await expect(page.getByText(/nie może być w przeszłości/i)).toBeVisible();
+  // Set the start date and try to go to the next step without setting the end date
+  await page.getByRole("button", { name: "Data i godzina" }).click();
+  await page.getByRole("button", { name: "Wednesday, August 13th," }).click();
+  await page.getByRole("button", { name: "Dalej" }).click();
+  // Verify the error message for invalid end date is displayed
+  await expect(page.getByText(/musi być po dacie rozpoczęcia/i)).toBeVisible();
+});
+
 test("should throw an error if the event slug is already used", async ({
   page,
 }) => {
