1ed5922b1b033b606281ed76371e13d0b6ea92d4
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2025-08-06 15:25:28 +0200

    feat: start implementing new landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,33 @@
-import React from "react";
-
-import { AuthButton } from "@/components/auth-button";
-import { AboutApp } from "@/components/landing/about-app";
-import { AboutUs } from "@/components/landing/about-us";
-import { Footer } from "@/components/landing/footer";
-import { Hero } from "@/components/landing/hero";
-import { Navbar } from "@/components/navbar";
+import { Navbar } from "@/components/landing/navbar";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <div className="container mx-auto">
-        <div className="bg-background absolute inset-x-0 z-50 container mx-auto min-w-screen">
-          <header className="container mx-auto flex justify-between p-4">
-            <Navbar authButton={<AuthButton />} />
-          </header>
-          <div className="container mx-auto flex p-4">
-            <div className="w-full px-2 sm:px-6 lg:px-8">
-              <Hero />
-              <AboutApp />
-            </div>
+    <div className="text-foreground min-h-screen">
+      <div className="flex w-full flex-col items-center gap-20 bg-gradient-to-tr from-[#E9EAEE] to-[#6f83af] pt-12 dark:from-[#101011] dark:to-[#213560]">
+        <Navbar />
+        <div className="flex flex-col items-center gap-8">
+          <div className="rounded-full bg-gradient-to-r from-[#6583C8] to-[#80B3FF] p-0.5">
+            <p className="flex h-full w-full rounded-full bg-[#a7b3cd] px-4 py-2 font-medium dark:bg-[#192237]">
+              Spraw sobie niezapomniane wydarzenia
+            </p>
           </div>
-          <AboutUs />
-          <Footer />
+          <p className="text-8xl font-bold uppercase">Eventownik Solvro</p>
+          <p className="text-secondary text-3xl">
+            Z nami zorganizujesz każde wydarzenie – łatwo, bez stresu i z
+            gwarancją sukcesu.
+          </p>
+          <div className="space-x-4">
+            <Button className="rounded-full px-5 py-4 text-lg font-medium">
+              Stwórz wydarzenie
+            </Button>
+            <Button
+              className="rounded-full border-2 bg-transparent px-5 py-4 text-lg font-medium"
+              variant={"outline"}
+            >
+              Przeglądaj wydarzenia
+            </Button>
+          </div>
         </div>
       </div>
     </div>
