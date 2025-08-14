05b28777260c4b43341c816549e4b9814a350788
Tymon Jędryczka <jedryczkatymon@gmail.com>
2025-08-14 16:34:17 +0200

    chore: unify color

--- src/app/dashboard/events/[id]/forms/delete-form-popup.tsx
+++ src/app/dashboard/events/[id]/forms/delete-form-popup.tsx
@@ -64,7 +64,7 @@ function DeleteFormPopup({
           <DialogTitle>Usuń formularz</DialogTitle>
         </div>
         <div className="flex flex-col items-center justify-center gap-4 text-center">
-          <CircleX className="text-destructive h-14 w-14" />
+          <CircleX className="h-14 w-14 text-[#EB0000]" />
           <p className="text-lg font-bold">Jesteś pewien?</p>
           <p className="text-sm">
             Czy na pewno chcesz usunąć formularz <strong>{formName}</strong>?
@@ -74,7 +74,7 @@ function DeleteFormPopup({
               <Button
                 type="submit"
                 variant="destructive"
-                className="bg-destructive"
+                className="bg-[#EB0000]"
                 disabled={form.formState.isSubmitting || shouldDisableButtons}
               >
                 {form.formState.isSubmitting || shouldDisableButtons ? (
