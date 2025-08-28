e13b01562a51afed4544b18e7774d54beb1bfe73
Maciej Malinowski <mejsiejdev@gmail.com>
2025-08-28 19:12:07 +0200

    feat: prepare navbar for mobile view

--- src/components/landing/navbar.tsx
+++ src/components/landing/navbar.tsx
@@ -1,4 +1,4 @@
-import { Bug, Star } from "lucide-react";
+import { Bug, Menu, Star } from "lucide-react";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 
@@ -14,39 +14,50 @@ export async function Navbar() {
     stargazers_count: number;
   };
   return (
-    <header className="bg-background border-border container flex w-full flex-row items-center justify-between gap-16 overflow-hidden rounded-2xl border p-3 xl:max-w-6xl">
-      <div className="flex items-center gap-8 uppercase">
-        <h1 className="sr-only">Eventownik</h1>
-        <AppLogo />
-        <Link href="/">Wydarzenia</Link>
-        <Link href="/">Funkcjonalności</Link>
-        <Link href="/">FAQ</Link>
-      </div>
-      <div className="flex items-center gap-4">
-        <Button variant="outline" asChild>
-          <a
-            target="_blank"
-            href="https://github.com/Solvro/web-eventownik-v2"
-            rel="noreferrer noopener"
-          >
-            <FaGithub />
-            <p>Daj nam gwiazdkę</p>
-            <Star fill="#3672FD" strokeWidth={0} size={20} />
-            <p className="font-medium">{stargazers_count}</p>
-          </a>
-        </Button>
-        <a
-          target="_blank"
-          href="https://solvro.pwr.edu.pl/contact/"
-          rel="noreferrer noopener"
-        >
-          <Button variant="outline">
-            <Bug />
-            Zgłoś błąd
+    <div className="flex w-full flex-col items-center px-8">
+      <header className="bg-background border-border container flex w-full flex-row items-center justify-between gap-4 rounded-2xl border p-3 xl:max-w-6xl">
+        <div className="flex items-center gap-8 uppercase">
+          <h1 className="sr-only">Eventownik</h1>
+          <AppLogo />
+          <Link href="/" className="hidden lg:block">
+            Wydarzenia
+          </Link>
+          <Link href="/" className="hidden lg:block">
+            Funkcjonalności
+          </Link>
+          <Link href="/" className="hidden lg:block">
+            FAQ
+          </Link>
+        </div>
+        <div className="flex items-center gap-4">
+          <Button variant="outline" className="hidden lg:block" asChild>
+            <a
+              target="_blank"
+              href="https://github.com/Solvro/web-eventownik-v2"
+              rel="noreferrer noopener"
+            >
+              <FaGithub />
+              <p>Daj nam gwiazdkę</p>
+              <Star fill="#3672FD" strokeWidth={0} size={20} />
+              <p className="font-medium">{stargazers_count}</p>
+            </a>
           </Button>
-        </a>
-        <ThemeSwitch />
-      </div>
-    </header>
+          <Button variant="outline" asChild className="hidden lg:block">
+            <a
+              target="_blank"
+              href="https://solvro.pwr.edu.pl/contact/"
+              rel="noreferrer noopener"
+            >
+              <Bug />
+              Zgłoś błąd
+            </a>
+          </Button>
+          <ThemeSwitch />
+          <Button variant="outline" className="block lg:hidden">
+            <Menu />
+          </Button>
+        </div>
+      </header>
+    </div>
   );
 }
