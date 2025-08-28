f400018abda97d68af7b883c1055aa0055d5fabb
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2025-08-28 12:57:11 +0200

    feat: add number of stargazers in the navbar

--- src/components/landing/navbar.tsx
+++ src/components/landing/navbar.tsx
@@ -1,11 +1,18 @@
-import { Bug, Github } from "lucide-react";
+import { Bug, Star } from "lucide-react";
 import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 import { AppLogo } from "../landing/app-logo";
 import { ThemeSwitch } from "../theme-switch";
 import { Button } from "../ui/button";
 
-export function Navbar() {
+export async function Navbar() {
+  const repositoryData = await fetch(
+    "https://api.github.com/repos/Solvro/web-eventownik-v2",
+  );
+  const { stargazers_count } = (await repositoryData.json()) as {
+    stargazers_count: number;
+  };
   return (
     <header className="bg-background border-border container flex w-full flex-row items-center justify-between gap-16 overflow-hidden rounded-2xl border p-3 xl:max-w-6xl">
       <div className="flex items-center gap-8 uppercase">
@@ -16,16 +23,18 @@ export function Navbar() {
         <Link href="/">FAQ</Link>
       </div>
       <div className="flex items-center gap-4">
-        <a
-          target="_blank"
-          href="https://github.com/Solvro/web-eventownik-v2"
-          rel="noreferrer noopener"
-        >
-          <Button variant="outline">
-            <Github />
-            Daj nam gwiazdkę
-          </Button>
-        </a>
+        <Button variant="outline" asChild>
+          <a
+            target="_blank"
+            href="https://github.com/Solvro/web-eventownik-v2"
+            rel="noreferrer noopener"
+          >
+            <FaGithub />
+            <p>Daj nam gwiazdkę</p>
+            <Star fill="#3672FD" strokeWidth={0} size={20} />
+            <p className="font-medium">{stargazers_count}</p>
+          </a>
+        </Button>
         <a
           target="_blank"
           href="https://solvro.pwr.edu.pl/contact/"
