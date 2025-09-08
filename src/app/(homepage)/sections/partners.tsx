3fa1609bc91207e1353a65744e644cc456b8d535
mejsiej <105872023+mejsiejdev@users.noreply.github.com>
2025-09-08 12:57:11 +0200

    feat: make partners section mobile friendly

--- src/app/(homepage)/sections/partners.tsx
+++ src/app/(homepage)/sections/partners.tsx
@@ -6,11 +6,13 @@ export function Partners() {
       id="partners"
       className="border-input z-10 flex w-full flex-col items-center border-y border-dashed bg-white dark:bg-[#101011]"
     >
-      <div className="divide-input container flex flex-row items-center justify-between divide-x divide-dashed">
-        <p className="px-16 py-8 text-2xl font-medium whitespace-nowrap uppercase">
-          Zaufani partnerzy
-        </p>
-        <div className="flex w-full flex-row items-center justify-evenly gap-16 px-16 py-8">
+      <div className="divide-input container flex w-full flex-col items-center justify-between divide-y divide-dashed sm:divide-x sm:divide-y-0 lg:flex-row">
+        <div className="flex h-full w-full flex-col items-center justify-center px-16 py-8 sm:w-auto">
+          <p className="h-full text-center text-2xl font-medium whitespace-nowrap uppercase sm:text-left">
+            Zaufani partnerzy
+          </p>
+        </div>
+        <div className="flex w-full flex-col items-center justify-evenly gap-16 px-16 py-8 lg:flex-row">
           <Image
             src={"/assets/logo/pwr.png"}
             alt={"Politechnika Wrocławska"}
