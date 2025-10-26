4b56be2191941b6d7cf8f87e02e3123acdb50656
Maciej Król <maciekgplay@gmail.com>
2025-10-26 13:40:52 +0100

    chore: add comment to trigger deployment

--- src/app/dashboard/(create-event)/actions.ts
+++ src/app/dashboard/(create-event)/actions.ts
@@ -27,6 +27,8 @@ export async function saveEvent(event: Event) {
   formData.append("slug", event.slug);
   formData.append("termsLink", event.termsLink ?? "");
 
+  // NOTE: Backend expects ISO 9075 format but returns ISO 8601 format for
+  // reasons unknown to anyone.
   formData.append(
     "startDate",
     formatISO9075(event.startDate, { representation: "complete" }),
