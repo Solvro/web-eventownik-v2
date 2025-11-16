81e645f378f1cac579bebe98f57656858af39e42
Maciej Król <maciekgplay@gmail.com>
2025-11-16 16:38:31 +0100

    chore: remove premiere announcement in dashboard

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -94,19 +94,6 @@ export default async function EventListPage() {
             ) : null}
           </div>
         </div>
-        <Alert>
-          <AlertCircleIcon />
-          <AlertTitle className="line-clamp-0">
-            Oficjalna premiera Eventownika już wkrótce!
-          </AlertTitle>
-          <AlertDescription className="text-foreground inline">
-            Jeśli chcesz stworzyć i udostępnić wydarzenie już teraz - odezwij
-            się do nas:{" "}
-            <a href="mailto:eventownik@pwr.edu.pl" className="hover:underline">
-              eventownik@pwr.edu.pl
-            </a>
-          </AlertDescription>
-        </Alert>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
           {events.length > 0 ? (
             events.map((event) => (
