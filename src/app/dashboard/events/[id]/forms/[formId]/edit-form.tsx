9898c959a34f881b90cd35250feedd39693bc949
Maciej Malinowski <mejsiejdev@gmail.com>
2025-12-16 19:24:31 +0100

    fix(forms): temporarily disable date selection (#252)
    * fix(forms): temporarily disable time selection
    
    * fix(create-event): allow start before yesterday
    
    * fix(forms): temporarily disable date selection

--- src/app/dashboard/events/[id]/forms/[formId]/edit-form.tsx
+++ src/app/dashboard/events/[id]/forms/[formId]/edit-form.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { format } from "date-fns";
+//import { format } from "date-fns";
 import {
-  CalendarArrowDownIcon,
-  CalendarArrowUpIcon,
+  //CalendarArrowDownIcon,
+  //CalendarArrowUpIcon,
   Loader,
   Save,
 } from "lucide-react";
@@ -15,7 +15,7 @@ import { z } from "zod";
 import { AttributesReorder } from "@/components/attributes-manager";
 import { WysiwygEditor } from "@/components/editor";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+//import { Calendar } from "@/components/ui/calendar";
 import {
   Form,
   FormControl,
@@ -26,11 +26,13 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+/*
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+*/
 import { Switch } from "@/components/ui/switch";
 import { UnsavedChangesAlert } from "@/components/unsaved-changes-alert";
 import { useToast } from "@/hooks/use-toast";
@@ -145,8 +147,9 @@ function EventFormEditForm({
                 </FormItem>
               )}
             />
+            {/*
             <div className="space-y-2">
-              <FormLabel>Data i godzina otwarcia</FormLabel>
+              <FormLabel>Data otwarcia</FormLabel>
               <div className="flex flex-row items-center gap-4">
                 <FormField
                   control={form.control}
@@ -174,10 +177,6 @@ function EventFormEditForm({
                             mode="single"
                             selected={field.value}
                             onSelect={field.onChange}
-                            disabled={(date: Date) =>
-                              new Date(date) <=
-                              new Date(form.getValues("startDate"))
-                            }
                           />
                         </PopoverContent>
                       </Popover>
@@ -210,7 +209,7 @@ function EventFormEditForm({
               </div>
             </div>
             <div className="space-y-2">
-              <FormLabel>Data i godzina zamknięcia</FormLabel>
+              <FormLabel>Data zamknięcia</FormLabel>
               <div className="flex flex-row items-center gap-4">
                 <FormField
                   control={form.control}
@@ -271,8 +270,10 @@ function EventFormEditForm({
                     </FormItem>
                   )}
                 />
+
               </div>
             </div>
+            */}
           </div>
           <div className="flex flex-col gap-8">
             <FormField
