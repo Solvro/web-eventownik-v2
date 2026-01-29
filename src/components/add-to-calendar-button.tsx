c16ff77e38183fbea840e8105e57ba2a113ac392
ShadowCatP <282276@student.pwr.edu.pl>
2026-01-29 23:32:08 +0100

    refactor: moved calendar related function to a lib folder

--- src/components/add-to-calendar-button.tsx
+++ src/components/add-to-calendar-button.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { createEvent } from "ics";
 import { CalendarPlus } from "lucide-react";
 import { useTranslations } from "next-intl";
-import React from "react";
 
 import { EventInfoDiv } from "@/components/event-info-div";
 import {
@@ -11,65 +9,8 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { downloadFile } from "@/lib/utils";
+import { addToGoogleCalendar, downloadICSFile } from "@/lib/calendar";
 import type { Event } from "@/types/event";
-
-function downloadICSFile(event: Event) {
-  const startDate = new Date(event.startDate);
-  const endDate = new Date(event.endDate);
-  const { error, value } = createEvent({
-    title: event.name,
-    description: event.description ?? undefined,
-    start: [
-      startDate.getFullYear(),
-      startDate.getMonth() + 1,
-      startDate.getDate(),
-      startDate.getHours(),
-      startDate.getMinutes(),
-    ],
-    end: [
-      endDate.getFullYear(),
-      endDate.getMonth() + 1,
-      endDate.getDate(),
-      endDate.getHours(),
-      endDate.getMinutes(),
-    ],
-    location: event.location ?? undefined,
-    status: "CONFIRMED",
-    url: `https://eventownik.solvro.pl/${event.slug}`,
-  });
-  if (error != null || value == null) {
-    console.error("Error creating ICS file:", error);
-    return;
-  }
-  downloadFile(
-    new Blob([value], { type: "text/calendar" }),
-    `${event.name.replaceAll(/[^a-zA-Z0-9]/g, "_")}.ics`,
-  );
-}
-
-function addToGoogleCalendar(event: Event) {
-  const startDate = new Date(event.startDate);
-  const endDate = new Date(event.endDate);
-
-  const formatDate = (date: Date) => {
-    return `${date.toISOString().replaceAll(/[-:]/g, "").split(".")[0]}Z`;
-  };
-
-  const parameters = new URLSearchParams({
-    action: "TEMPLATE",
-    text: event.name,
-    dates: `${formatDate(startDate)}/${formatDate(endDate)}`,
-    details: event.description ?? "",
-    location: event.location ?? "",
-    sprop: `website:https://eventownik.solvro.pl/${event.slug}`,
-  });
-
-  window.open(
-    `https://calendar.google.com/calendar/render?${parameters.toString()}`,
-    "_blank",
-  );
-}
 
 export function AddToCalendarButton({ event }: { event: Event }) {
   const t = useTranslations("Event");
