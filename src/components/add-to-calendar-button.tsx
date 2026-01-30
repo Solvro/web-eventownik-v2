1e5148a0a05b31d48ca25b607fd3be56a6fe3d43
Bohdan Koshkin <153859662+ShadowCatP@users.noreply.github.com>
2026-01-30 20:11:32 +0100

    feat: better 'add to calendar' options (#284)
    * feat: added popover and google calendar option
    
    * refactor: moved calendar related function to a lib folder
    
    * refactor: made dialog instead of popover and added icons + translations
    
    * refactor: changed dialog layout
    
    * refactor: added apple option and changed layout spacing
    
    * refactor: added sanitization of ics files event description
    
    * refactor: improved responsiveness
    
    * refactor: removed download .ics button

--- src/components/add-to-calendar-button.tsx
+++ src/components/add-to-calendar-button.tsx
@@ -1,60 +1,107 @@
 "use client";
 
-import { createEvent } from "ics";
-import { CalendarPlus } from "lucide-react";
+import { format } from "date-fns";
+import { CalendarClock, CalendarPlus, MapPin } from "lucide-react";
 import { useTranslations } from "next-intl";
-import React from "react";
+import Image from "next/image";
 
 import { EventInfoDiv } from "@/components/event-info-div";
-import { downloadFile } from "@/lib/utils";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { addToGoogleCalendar, downloadICSFile } from "@/lib/calendar";
 import type { Event } from "@/types/event";
 
-function downloadICSFile(event: Event) {
-  const startDate = new Date(event.startDate);
-  const endDate = new Date(event.endDate);
-  const { error, value } = createEvent({
-    title: event.name,
-    description: event.description ?? undefined,
-    start: [
-      startDate.getFullYear(),
-      startDate.getMonth() + 1,
-      startDate.getDate(),
-      startDate.getHours(),
-      startDate.getMinutes(),
-    ],
-    end: [
-      endDate.getFullYear(),
-      endDate.getMonth() + 1,
-      endDate.getDate(),
-      endDate.getHours(),
-      endDate.getMinutes(),
-    ],
-    location: event.location ?? undefined,
-    status: "CONFIRMED",
-    url: `https://eventownik.solvro.pl/${event.slug}`,
-  });
-  if (error != null || value == null) {
-    console.error("Error creating ICS file:", error);
-    return;
-  }
-  downloadFile(
-    new Blob([value], { type: "text/calendar" }),
-    `${event.name.replaceAll(/[^a-zA-Z0-9]/g, "_")}.ics`,
-  );
-}
+import { Button } from "./ui/button";
 
 export function AddToCalendarButton({ event }: { event: Event }) {
   const t = useTranslations("Event");
   return (
-    <EventInfoDiv
-      onClick={(event_) => {
-        event_.preventDefault();
-        downloadICSFile(event);
-      }}
-      className="hover:bg-accent/20 cursor-pointer"
-    >
-      <CalendarPlus size={20} />
-      <span className="hidden md:inline"> {t("addToCalendar")}</span>
-    </EventInfoDiv>
+    <Dialog>
+      <DialogTrigger asChild>
+        <EventInfoDiv className="hover:bg-accent/20 cursor-pointer">
+          <CalendarPlus size={20} />
+          <span className="hidden md:inline"> {t("addToCalendar")}</span>
+        </EventInfoDiv>
+      </DialogTrigger>
+      <DialogContent className="w-95/100 max-w-md">
+        <DialogTitle></DialogTitle>
+        <div className="space-y-6">
+          <div className="grid">
+            <span className="break mb-2 max-w-80 truncate text-3xl font-bold">
+              {event.name}
+            </span>
+            <div className="flex items-start gap-3">
+              <CalendarClock
+                size={60}
+                className="text-muted-foreground shrink-0 stroke-1"
+              />
+              <div>
+                <div className="font-medium">
+                  {format(event.startDate, "dd.MM.yyyy")} -{" "}
+                  {format(event.endDate, "dd.MM.yyyy")}
+                </div>
+                <div className="text-muted-foreground text-sm">
+                  {format(event.startDate, "HH.mm")} -{" "}
+                  {format(event.endDate, "HH:mm")}
+                </div>
+              </div>
+            </div>
+
+            {event.location == null ? null : (
+              <div className="mt-4 flex items-start gap-2">
+                <MapPin
+                  size={20}
+                  className="text-muted-foreground mt-0.5 shrink-0"
+                />
+                <span className="text-muted-foreground line-clamp-2">
+                  {event.location}
+                </span>
+              </div>
+            )}
+          </div>
+
+          <div className="grid grid-cols-2 gap-2">
+            <Button
+              onClick={(event_) => {
+                event_.preventDefault();
+                addToGoogleCalendar(event);
+              }}
+              variant={"outline"}
+              className="grow justify-start"
+            >
+              <Image
+                src="/assets/logo/googlecalendar.svg"
+                alt="Google Calendar"
+                className="dark:invert"
+                width={20}
+                height={20}
+              />
+              Google
+            </Button>
+            <Button
+              onClick={(event_) => {
+                event_.preventDefault();
+                downloadICSFile(event);
+              }}
+              variant={"outline"}
+              className="grow justify-start"
+            >
+              <Image
+                src="/assets/logo/apple.svg"
+                alt="Apple"
+                className="dark:invert"
+                width={20}
+                height={20}
+              />
+              Apple
+            </Button>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
