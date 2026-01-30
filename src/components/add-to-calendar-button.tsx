4e4fb1f8ebeaacd7ce7d25d670a1c737a29dccc5
ShadowCatP <282276@student.pwr.edu.pl>
2026-01-30 16:55:39 +0100

    refactor: added apple option and changed layout spacing

--- src/components/add-to-calendar-button.tsx
+++ src/components/add-to-calendar-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { CalendarPlus, Download } from "lucide-react";
+import { format } from "date-fns";
+import { CalendarClock, CalendarPlus, Download, MapPin } from "lucide-react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 
@@ -26,9 +27,38 @@ export function AddToCalendarButton({ event }: { event: Event }) {
           <span className="hidden md:inline"> {t("addToCalendar")}</span>
         </EventInfoDiv>
       </DialogTrigger>
-      <DialogContent className="max-w-75 md:max-w-100">
-        <DialogTitle>{t("addToCalendar")}</DialogTitle>
-        <div className="space-y-2">
+      <DialogContent className="max-w-85 md:max-w-100">
+        <DialogTitle></DialogTitle>
+        <div className="-mt-8 space-y-6">
+          <div className="grid">
+            <span className="break mb-2 max-w-80 text-3xl font-bold">
+              {event.name}
+            </span>
+            <div className="flex items-start gap-3">
+              <CalendarClock
+                size={60}
+                className="text-muted-foreground shrink-0"
+              />
+              <div>
+                <div className="font-medium">
+                  {format(event.startDate, "dd.MM.yyyy")} -{" "}
+                  {format(event.endDate, "dd.MM.yyyy")}
+                </div>
+                <div className="text-muted-foreground text-sm">
+                  {format(event.startDate, "HH.mm")} -{" "}
+                  {format(event.endDate, "HH:mm")}
+                </div>
+              </div>
+            </div>
+
+            {event.location == null ? null : (
+              <div className="mt-4 flex items-center gap-2">
+                <MapPin size={20} className="text-muted-foreground shrink-0" />
+                <span className="text-muted-foreground">{event.location}</span>
+              </div>
+            )}
+          </div>
+
           <div className="grid gap-2 md:grid-cols-2">
             <Button
               onClick={(event_) => {
@@ -54,6 +84,23 @@ export function AddToCalendarButton({ event }: { event: Event }) {
               }}
               variant={"outline"}
               className="grow justify-start"
+            >
+              <Image
+                src="/assets/logo/apple.svg"
+                alt="Apple"
+                className="invert"
+                width={20}
+                height={20}
+              />
+              Apple
+            </Button>
+            <Button
+              onClick={(event_) => {
+                event_.preventDefault();
+                downloadICSFile(event);
+              }}
+              variant={"outline"}
+              className="col-span-2 grow justify-start"
             >
               <Download size={20} />
               {t("download")}
