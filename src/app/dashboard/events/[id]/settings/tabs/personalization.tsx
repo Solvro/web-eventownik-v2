5b236a63cae2bf5f1e9620fbeae2cc169dccfc22
Bohdan Moshenets <bogdan.moshenets@outlook.com>
2026-02-05 12:07:33 +0100

    fix: improve responsiveness and active state handling in the WYSIWYG editor (#292)
    * fix(event-settings): improve responsiveness of GeneralInfoForm and PersonalizationForm
    
    * fix(editor-menu-bar): enhance responsiveness and state management for WYSIWYG editor

--- src/app/dashboard/events/[id]/settings/tabs/personalization.tsx
+++ src/app/dashboard/events/[id]/settings/tabs/personalization.tsx
@@ -1,5 +1,7 @@
 import { PersonalizationForm } from "@/components/forms/event/personalization-form";
 
 export function Personalization() {
-  return <PersonalizationForm className="md:w-3xl" />;
+  return (
+    <PersonalizationForm className="max-w-3xl md:grid-cols-1 lg:grid-cols-2" />
+  );
 }
---
+++ src/types/editor-active-state.ts
@@ -0,0 +1,14 @@
+export interface EditorActiveState {
+  bold: boolean;
+  italic: boolean;
+  code: boolean;
+  heading1: boolean;
+  heading2: boolean;
+  heading3: boolean;
+  bulletList: boolean;
+  orderedList: boolean;
+  alignLeft: boolean;
+  alignCenter: boolean;
+  alignRight: boolean;
+  alignJustify: boolean;
+}
