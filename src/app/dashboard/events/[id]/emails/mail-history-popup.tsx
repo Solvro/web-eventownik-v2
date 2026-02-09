afbbb5701c3a1bd0cf7ffe2e8b832add361bb525
Maciej Malinowski <mejsiejdev@gmail.com>
2026-02-09 12:00:51 +0100

    feat: use credenza for main history popup

--- src/app/dashboard/events/[id]/emails/mail-history-popup.tsx
+++ src/app/dashboard/events/[id]/emails/mail-history-popup.tsx
@@ -4,29 +4,31 @@ import { History } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+  Credenza,
+  CredenzaContent,
+  CredenzaTitle,
+  CredenzaTrigger,
+} from "@/components/ui/credenza";
 import type { SingleEventEmail } from "@/types/emails";
 
 import { EmailHistoryTable } from "./(table)/table";
 
 function MailHistoryPopup({ email }: { email: SingleEventEmail }) {
   return (
-    <Dialog>
-      <DialogTrigger asChild>
+    <Credenza>
+      <CredenzaTrigger asChild>
         <Button variant="eventGhost" size="icon" title="Historia wiadomości">
           <History />
           <span className="sr-only">Historia wiadomości</span>
         </Button>
-      </DialogTrigger>
-      <DialogContent className="max-h-96 max-w-128">
-        <DialogTitle>Historia wiadomości</DialogTitle>
-        <EmailHistoryTable email={email} />
-      </DialogContent>
-    </Dialog>
+      </CredenzaTrigger>
+      <CredenzaContent className="max-h-96 max-w-3xl">
+        <div className="space-y-4 p-6 md:p-0">
+          <CredenzaTitle>Historia wiadomości</CredenzaTitle>
+          <EmailHistoryTable email={email} />
+        </div>
+      </CredenzaContent>
+    </Credenza>
   );
 }
 
