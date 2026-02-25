00005dcf7a9d8a2d937f4a7925157ce9d09a8ece
ShadowCatP <282276@student.pwr.edu.pl>
2026-02-25 22:21:34 +0100

    feat: add column visibility dropdown

--- src/app/dashboard/events/[id]/participants/table/core/participants-table.tsx
+++ src/app/dashboard/events/[id]/participants/table/core/participants-table.tsx
@@ -25,9 +25,10 @@ declare module "@tanstack/react-table" {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue> {
     attribute?: Attribute;
+    name?: string;
     headerClassName?: string;
     cellClassName?: string;
-    showInTable: boolean;
+    showInTable?: boolean;
   }
 
   interface TableMeta<TData extends RowData> {
