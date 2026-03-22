6ea2978abeed1f1d6c6a2def701a3ca67013d7fd
Maciej Malinowski <mejsiejdev@gmail.com>
2026-03-22 20:41:34 +0100

    feat: display user identity in block participants (#327)
    * feat: display user identity in block participants
    
    * feat: display user identity in block participants
    
    * fix: remove unused type from sortable block grid

--- src/types/blocks.ts
+++ src/types/blocks.ts
@@ -11,6 +11,7 @@ export interface Block {
   isRootBlock: boolean;
   children: Block[];
   meta: {
+    participants: BlockParticipant[];
     participantsInBlockCount: number | undefined;
   };
 }
