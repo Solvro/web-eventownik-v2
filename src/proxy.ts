ac467ebac5ba39c162c7efabf7728601c5808203
Stanisław Synowiec <stasyn1410@gmail.com>
2026-04-28 10:21:44 +0200

    fix: proxy.ts file location - move logic from /proxy.ts to /src/proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,6 +4,13 @@ import { NextResponse } from "next/server";
 import { verifySession } from "@/lib/session";
 
 export async function proxy(request: NextRequest) {
+  const url = request.nextUrl.clone();
+
+  if (url.pathname.includes("//")) {
+    url.pathname = url.pathname.replaceAll(/\/+/g, "/");
+    return NextResponse.redirect(url);
+  }
+
   const session = await verifySession();
 
   if (
@@ -17,7 +24,3 @@ export async function proxy(request: NextRequest) {
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/dashboard/:path*"],
-};
