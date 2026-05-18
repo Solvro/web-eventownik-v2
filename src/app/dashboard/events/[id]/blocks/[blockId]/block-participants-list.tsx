81705f1f95f21ee465e337aba3a3edc5630f89c0
Stanisław Synowiec <stasyn1410@gmail.com>
2026-05-18 14:05:21 +0200

    fix: remove render empty ul if participatns length === 0

--- src/app/dashboard/events/[id]/blocks/[blockId]/block-participants-list.tsx
+++ src/app/dashboard/events/[id]/blocks/[blockId]/block-participants-list.tsx
@@ -13,18 +13,19 @@ export function BlockParticipantsList({
   const t = useTranslations("Form");
   return (
     <ScrollArea className="h-50 w-full rounded-md border p-4">
-      {participants.length === 0 && (
+      {participants.length === 0 ? (
         <div className="flex h-41 w-full items-center justify-center">
           {t("noParticipants")}
         </div>
+      ) : (
+        <ul>
+          {participants.map((participant) => (
+            <li key={participant.id} className="py-1 text-left text-sm">
+              {participant.name}
+            </li>
+          ))}
+        </ul>
       )}
-      <ul>
-        {participants.map((participant) => (
-          <li key={participant.id} className="py-1 text-left text-sm">
-            {participant.name}
-          </li>
-        ))}
-      </ul>
     </ScrollArea>
   );
 }
