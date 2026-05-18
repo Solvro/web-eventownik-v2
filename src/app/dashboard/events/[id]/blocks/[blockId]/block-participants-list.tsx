2147f3cb84900b48433b0ccdcad175a146d54f3d
Stanisław Synowiec <stasyn1410@gmail.com>
2026-05-18 21:02:45 +0200

    feat(dashboard): add redesigned blocks (#358)
    * feat(components): add progress bar shadcn/ui components
    
    * feat(blocks): new block design
    - scrollable participants list
    - toggle participants visiblity
    - percent occupancy bar
    
    * fix(blocks): round block occupancy
    
    * fix(blocks): round block occupancy
    
    * fix(blocks): remove block popup (wait for v3 backend)
    
    * fix: useAtomValue
    
    Co-authored-by: Maciej Król <63610278+maciejkrol18@users.noreply.github.com>
    
    * fix: useAtomValue
    
    Co-authored-by: Maciej Król <63610278+maciejkrol18@users.noreply.github.com>
    
    * fix(config): remove packageManager
    
    * fix: remove render empty ul if participatns length === 0
    
    * fix: infinity block space
    
    * fix: "Bez limitu" text
    
    * fix(style): safe natural text-left
    
    * fix(style): center button text
    
    * fix(style): tablet resolution
    
    ---------
    
    Co-authored-by: Maciej Król <63610278+maciejkrol18@users.noreply.github.com>

---
+++ src/app/dashboard/events/[id]/blocks/[blockId]/block-participants-list.tsx
@@ -0,0 +1,31 @@
+import { useTranslations } from "next-intl";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import type { BlockParticipant } from "@/types/blocks";
+
+interface BlockParticipantsListProps {
+  participants: BlockParticipant[];
+}
+
+export function BlockParticipantsList({
+  participants,
+}: BlockParticipantsListProps) {
+  const t = useTranslations("Form");
+  return (
+    <ScrollArea className="h-50 w-full rounded-md border p-4">
+      {participants.length === 0 ? (
+        <div className="flex h-41 w-full items-center justify-center">
+          {t("noParticipants")}
+        </div>
+      ) : (
+        <ul>
+          {participants.map((participant) => (
+            <li key={participant.id} className="py-1 text-sm">
+              {participant.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </ScrollArea>
+  );
+}
