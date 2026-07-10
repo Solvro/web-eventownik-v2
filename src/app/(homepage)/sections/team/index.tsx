f383470c8edd2e3caf13fa52aa78e0393245978a
maks1u <smorldesign@gmail.com>
2026-07-10 23:38:19 +0200

    fix: wrong div with overflow-hidden

--- src/app/(homepage)/sections/team/index.tsx
+++ src/app/(homepage)/sections/team/index.tsx
@@ -290,9 +290,9 @@ export async function Team() {
   return (
     <section
       id="team"
-      className="border-input container mb-16 flex flex-col items-center gap-8 rounded-b-4xl border-x border-b border-dashed bg-radial from-[#366CC8]/50 to-transparent px-8 pt-20 pb-12 sm:gap-24 sm:pb-20"
+      className="border-input container mb-16 flex flex-col items-center gap-8 rounded-b-4xl border-x border-b border-dashed bg-radial from-[#366CC8]/50 to-transparent px-8 pt-20 pb-12 max-sm:overflow-hidden sm:gap-24 sm:pb-20"
     >
-      <div className="flex flex-col items-center gap-8 overflow-hidden">
+      <div className="flex flex-col items-center gap-8">
         <p className="w-min rounded-full border border-[#6583C8] px-5 py-2 text-xl font-medium whitespace-nowrap text-[#6583C8]">
           Solvro Team
         </p>
